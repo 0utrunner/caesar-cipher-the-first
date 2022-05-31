22464ff28912ac4096a252dc819f3c62789baa86
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-31 16:41:18 -0500

    Js SOLVED

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,3 +1,39 @@
-exports.caesarCipher = function() {
+exports.caesarCipher = function(str, num) {
+    let copy = str.toUpperCase()
+    let indexes = []
+    let shifter = []
+    let numCounter = 0
+    let upper = ['A', 'B', 'C', 'D','E', 'F', 'G', 'H',
+    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',   'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  
+    for(let i = 0; i < copy.length; i++){
+      let same = (element) => element == copy[i];
+      if(upper.findIndex(same) == -1){
+        indexes.push(copy[i])
+      } else
+      indexes.push(upper.findIndex(same))
+    }
+  
+    if(num > 0){
+        while(numCounter != num){
+          upper.push(upper.shift())
+          numCounter++
+        }
+      }else{
+        while(numCounter != num){
+          upper.unshift(upper.pop())
+          numCounter--
+        }
+      }
+  
+    for(let i = 0; i < indexes.length; i++){
+      if(upper[indexes[i]] == undefined || typeof indexes[i] == 'string'){
+        shifter.push(indexes[i])
+      } else if (str[i] == str[i].toLowerCase()){
+    shifter.push(upper[indexes[i]].toString().toLowerCase())
+      } else
+      shifter.push(upper[indexes[i]])
+    }
 
+    return shifter.join(''); 
 };
